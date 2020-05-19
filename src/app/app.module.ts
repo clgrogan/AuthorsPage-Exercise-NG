7854c1b9e8b5c8868bbaee7e61019f74c681f884
Curt Grogan <curtgrogan@outlook.com>
2020-05-19 16:51:00 -0400

    Completed coding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { AuthorsComponent } from './authors/authors.component';
+import { AppComponent } from "./app.component";
+import { AuthorsComponent } from "./authors/authors.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AuthorsComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, AuthorsComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/authors.service.ts
+++ src/app/authors.service.ts
@@ -1,9 +1,16 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class AuthorsService {
+  authors = ["Author One", "Author Two", "Author Three"];
 
-  constructor() { }
+  getAuthors = () => {
+    console.log("Service authors: " + this.authors);
+    return ["Author One", "Author Two", "Author Three"];
+    // return "something";
+  };
+
+  constructor() {}
 }
