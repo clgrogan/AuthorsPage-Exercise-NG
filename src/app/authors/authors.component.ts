907208769f1154a18077187c81e86ee3a2e58625
Curt Grogan <curtgrogan@outlook.com>
2020-05-19 13:59:11 -0400

    Updated custom component. updated app.comp.html

--- src/app/authors/authors.component.ts
+++ src/app/authors/authors.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-authors',
-  templateUrl: './authors.component.html',
-  styleUrls: ['./authors.component.scss']
+  selector: "app-authors",
+  template: `<p>test</p>`,
+  styleUrls: ["./authors.component.scss"],
 })
 export class AuthorsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
